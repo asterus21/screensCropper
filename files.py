348b0fa7b9ea09b5be03e76d76e25581479ef345
Michael Paul <michaelpaulwem@gmail.com>
2025-12-21 00:16:52 +0300

    Files list check edited

--- files.py
+++ files.py
@@ -11,7 +11,11 @@ def get_time():
 
 def get_input() -> str:
     files = lambda folder: [file for file in os.listdir(folder) if file.lower().endswith('.png')]
-    is_empty = lambda files_list: print('The list of files to process is empty. The program is about to close.') and sys.exit(0) if not files_list else files_list
+    def is_empty(files_list):
+        if not files_list:
+            print('The list of files to process is empty. The program is about to close.')
+            sys.exit(1)        
+        else: return files_list
     user_input = input('Enter a path to the PNG files to crop (e.g. D:/screens) or press Enter to use a current directory (type exit to quit): \n')
     match user_input:
         case 'exit':
@@ -22,5 +26,5 @@ def get_input() -> str:
             return is_empty(files(user_input)) if user_input else is_empty(files(os.getcwd()))
     if not os.path.isdir(user_input):
         print(get_time(), 'Folder does not exist. The program is about to close.')
-        sys.exit(0)    
+        sys.exit(0)
     return user_input
